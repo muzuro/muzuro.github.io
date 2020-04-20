1aec33e72e721c2ea906754ddf2ba14865f6b42e
Zufar Muhamadeev <zufarm@gmail.com>
2020-04-20 17:40:18 +0300

    create page

---
+++ stripe-font-issue/js/example.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    var elements = stripe.elements({
+        fonts: [
+            {
+                cssSrc: 'http://db.onlinewebfonts.com/c/0abeb2471faeb5269db428b9eac2075e?family=GT+Walsheim+Pro+Regular',
+            },
+        ],
+    });
+    var elementStyles = {
+        base: {
+            fontFamily: 'GT Walsheim Pro Regular',
+            fontSize: '25px',
+        },
+    };
+    var cardNumber = elements.create('cardNumber', {
+        style: elementStyles,
+    });
+    cardNumber.mount('#example-card-number');
+})();
